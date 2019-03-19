243cfabb6471db0363f09ab3842509476e62c10a
mpabarca <mpabarca@uc.cl>
2019-03-19 12:35:58 -0300

    se crea codigo autenticacion en app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css';+
+import {fire , store } from './config/Fire';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user:{},
+    }
+  }
+
+  ComponentDidMount(){
+    this.authListener();
+  }
+
+  authListener(){
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user){
+        this.setState({ user });
+        localStorage.removeItem('user');
+      } else {
+        this.setState({ user: null});
+        localStorage.removeItem('user');
+      }
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        
+        {this.state.user ? (<Home />) : (<Login />)}        
       </div>
     );
   }
