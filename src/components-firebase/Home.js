564c08289c3a439bd878ac6b069d033b1894f3e7
mpabarca <mpabarca@uc.cl>
2019-03-19 22:45:20 -0300

    codigo de home y login hechos y funcionando

--- src/components-firebase/Home.js
+++ src/components-firebase/Home.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import fire from './Fire';
 
 class Home extends Component {
     constructor(props){
         super(props);
+        this.logout = this.logout.bind(this);
     }
+
+    logout(){
+        fire.auth().signOut();
+    }
+    
     render(){
         return(
             <div className="col-md-6">
--- src/components-firebase/Login.js
+++ src/components-firebase/Login.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import fire from './Fire';
 
 class Login extends Component {
     constructor(props) {
       super(props);
+      this.login = this.login.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.signup = this.signup.bind(this);
       this.state = {
           email: '',
           password: '',
 
       }
-    
-      }
-    
+    }
+    login(event){
+      event.preventDefault();
+      fire.auth().signInWithEmailAndPassword(this.state.email , this.state.password).then((u)=>{
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+    signup(event){
+      event.preventdefault();
+      fire.auth().createUserWithEmailAndPassword(this.state.email , this.state.password).then((u)=>{
+      }).then((u)=>{console.log(u)})
+      .catch((error)=>{
+        console.log(error);
+      })
+    }
   
-    handleChange(e) {
-      this.setState({ [e.target.name]: e.target.value });
+    handleChange(event) {
+      this.setState({ [event.target.name]: event.target.value });
     }
   
     render() {
