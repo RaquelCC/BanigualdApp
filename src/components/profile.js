aaea23835582776cce052d36665aea4314e7114e
RaquelCC <rcanalesconcha@gmail.com>
2019-03-21 16:33:37 -0300

    arreglando perfil

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,50 +3,28 @@ import './profile.css';
 import ProductsDisplay from './productsDisplay';
 import phoneIcon from '../img/fab call button.png';
 
-function Profile(props) { // en las props debería estar el perfil del usuario
-    // {
-    //     nombreEmprendedor: '',
-    //     fotoPerfil: '',
-    //     nombreEmprendimiento: '',
-    //     ubicacion: '',
-    //     contacto: {
-    //         whatsapp: '+569123456789',
-    //         mail: '',
-    //     },
-    //     rubro: '',
-    //     intereses: ['', ''],
-    //     ciclo: 0,
-    //     infoEmprendimiento: '',
-    //     productosOServicios: [{
-    //         nombreProducto: '',
-    //         foto: '',
-    //         precio: '',
-    //     }]
-    // }
-
-
+function Profile(props) { 
 
 
     return (
         <div>
             <div className="profile-container row">
-                <div className="col s12 center container-imagen-emprendedor">
-                    <img className="imagen-emprendedor responsive-img" src={props.perfil.fotoPerfil} alt="foto perfil"></img>
+                <div className="col s12 center container-imagen-emprendedor" style={{backgroundImage: "url('"+props.perfil.fotoPerfil+"')"}}>
                     <div className="overlay col s12">
                         <p className="nombre-emprendedor">{` ${props.perfil.nombreEmprendedor} `}</p>
                         <p className="ubicacion">{props.perfil.ubicacion}</p>
                     </div>
-                   <a href={`tel:${props.perfil.contacto.whatsapp}`} ><img className="call-icon" src={phoneIcon} alt="icon"></img></a>
+                    <a href={`tel:${props.perfil.contacto.whatsapp}`} ><img className="call-icon" src={phoneIcon} alt="icon"></img></a>
                 </div>
                 <div className="col s12 sub-info-emprendedor center">
                     <div className="rubro"><p>{props.perfil.rubro}</p></div>
                     <div className="ciclo"><p>{`Ciclo ${props.perfil.ciclo}`}</p></div>
                 </div>
                 <div className="col s12">
-                <p className="info-emprendimiento">{props.perfil.infoEmprendimiento}</p>
+                    <p className="info-emprendimiento">{props.perfil.infoEmprendimiento}</p>
                 </div>
                 <div className="col s12 center">
-                <div className="line"></div>
+                    <div className="line"></div>
                 </div>
             </div>
             <div className="products-container">
